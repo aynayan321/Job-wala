2f8170e434d3ffb30a1bcb7553f055604c5e3d8b
Mohd Ayan <mohdayanrizvi@gmail.com>
2026-04-30 20:25:36 +0530

    Update Footer.jsx

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,23 +1,40 @@
-import React, { useContext } from 'react'
-import {Context} from "../../main"
-import {Link} from "react-router-dom"
-import { FaGithub , FaLinkedin} from "react-icons/fa"
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
-import { RiInstagramFill} from "react-icons/ri"
+import { RiInstagramFill } from "react-icons/ri";
+
 function Footer() {
-  const {isAuthorized}  = useContext(Context)
+  const { isAuthorized } = useContext(Context);
+
   return (
-    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
-<div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
-</div>
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
       
+      {/* Change your name here */}
+      <div>&copy; {new Date().getFullYear()} Job-Wala. All rights reserved.</div>
+
+      <div>
+        {/* Replace with YOUR links */}
+        <a href="https://github.com/aynayan321" target="_blank" rel="noreferrer">
+          <FaGithub />
+        </a>
+
+        <a href="#" target="_blank" rel="noreferrer">
+          <SiLeetcode />
+        </a>
+
+        <a href="#" target="_blank" rel="noreferrer">
+          <FaLinkedin />
+        </a>
+
+        <a href="#" target="_blank" rel="noreferrer">
+          <RiInstagramFill />
+        </a>
+      </div>
+
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
